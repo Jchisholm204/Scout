415173a953d26d49627c3ae3873e3b9c00cc6279
Hendrix Gryspeerdt <95045551+hendrixgg@users.noreply.github.com>
2026-01-16 19:32:27 -0500

    able to send RPLidar GET_INFO and get response descriptor

--- src/control_board/firmware/core/src/protocols/rplidar/rplidar.c
+++ src/control_board/firmware/core/src/protocols/rplidar/rplidar.c
@@ -10,6 +10,8 @@
  */
 
 #include "protocols/rplidar/rplidar.h"
+#include "os/systime.h"
+#include "string.h"
 
 void vRpLidar_tsk(void* pvParams);
 
@@ -17,18 +19,16 @@ eRpLidarError rplidar_init(RpLidar_t* pHndl,
                            Serial_t* pSerial,
                            pin_t stx,
                            pin_t srx) {
-#warning "RpLiDAR Task not completed"
 
     // Perform initial checks
     if (!pHndl)
         return eRpLidarNULL;
     if (!pSerial)
         return eRpLidarNULL;
-    if (pSerial->state != eSerialNoInit) {
-        pHndl->state = eRpLidarInitFail;
-        return pHndl->state;
-    }
 
+    pHndl->pSerial = pSerial;
+
+    // designate memory for buffer
     pHndl->rx_hndl = xStreamBufferCreateStatic(RPLIDAR_BUF_LEN,
                                                1 /*!!change this!!*/,
                                                pHndl->rx_buf,
@@ -37,6 +37,12 @@ eRpLidarError rplidar_init(RpLidar_t* pHndl,
         pHndl->state = eRpLidarInitFail;
         return pHndl->state;
     }
+
+    // tell serial interface that the buffer exists
+    if (serial_attach(pHndl->pSerial, pHndl->rx_hndl) != eSerialOK) {
+        printf("cannot attach Serial3\n");
+    }
+
 
     // Init RpLidar Task
     pHndl->tsk_hndl = xTaskCreateStatic(vRpLidar_tsk,
@@ -76,7 +82,10 @@ eRpLidarError rplidar_read(RpLidar_t* pHndl, RpLidarScan* const pScan) {
 }
 
 void vRpLidar_tsk(void* pvParams){
-    
+    RpLidar_t* pHndl = pvParams;
+    if (!pHndl) {
+        vTaskSuspend(NULL);
+    }
     // send a GET_INFO request
     // output to stdout in debug mode to see sent request
     // wait
@@ -85,8 +94,40 @@ void vRpLidar_tsk(void* pvParams){
     // wait
     // receive GET_INFO data response
     // output to stdout in debug mode to see response
+    
+    char request[16];
+    uint8_t response[32];
+    request[0] = 0xA5;
+    request[1] = 0x50;
+    if (serial_write(pHndl->pSerial, request, 2, 10) != eSerialOK){
+        printf("cannot write GET_INFO request to Serial3\n");
+    }
+    printf("wrote GET_INFO request to Serial3\n");
 
+    printf("reading from Serial3:\n");
+    for (int i = 0; i < 7;) {
+        if (xStreamBufferReceive(pHndl->rx_hndl, response+i, 1, 10)) {
+            printf("byte %d: %X\n", i, response[i]);
+            i++;
+        }
+    }
+    
+    if (!(response[0]==RPLIDAR_RESPONSE_DESCRIPTOR.start_flag1 && response[1]==RPLIDAR_RESPONSE_DESCRIPTOR.start_flag2)) {
+        printf("format error on RPLidar response");
+    }
+    printf("GET_INFO response descriptor: %X %X %X %X %X %X %X\n", response[0], response[1], response[2], response[3], response[4], response[5], response[6]);
 
+    RpLidarResponseDescriptor get_info_response_descriptor;
+    memcpy(&get_info_response_descriptor, response, sizeof(RpLidarResponseDescriptor));
+    // TODO: print stuff from get_info_response_descriptor to make sure we are parsing correctly
+
+    uint32_t data_response_length = get_info_response_descriptor.data_response_detail & 0xFFFFFFFC;
+    for (uint32_t i = 0; i < data_response_length;) {
+        if (xStreamBufferReceive(pHndl->rx_hndl, response+i, 1, 10)) {
+            printf("byte %lu: %X\n", i, response[i]);
+            i++;
+        }
+    }
 
     // send a GET_HEALTH request
     // wait
@@ -96,4 +137,9 @@ void vRpLidar_tsk(void* pvParams){
 
 
     
+    TickType_t last_wake_time = xTaskGetTickCount();
+    for (;;) {
+        printf("vRpLidar_tsk\n");
+        vTaskDelayUntil(&last_wake_time, 1000);
+    }
 }
