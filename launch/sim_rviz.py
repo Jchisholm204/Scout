921165766b9fb8aa84bfc40d027947e5f73c8ae1
Jacob Chisholm <jacobchisholm1010@gmail.com>
2026-02-04 14:48:50 -0500

    laser segmentation

--- launch/sim_rviz.py
+++ launch/sim_rviz.py
@@ -1,6 +1,7 @@
 from launch import LaunchDescription
+from launch.actions import TimerAction, ExecuteProcess
 from launch.actions import DeclareLaunchArgument
-from launch_ros.actions import Node
+from launch_ros.actions import Node, LifecycleNode
 from ament_index_python.packages import get_package_share_directory
 import os
 
@@ -12,6 +13,65 @@ def generate_launch_description():
 
     with open(urdf_path, 'r') as f:
         robot_desc = f.read()
+
+    # 1. Define the node
+    seg_node = LifecycleNode(
+        package='laser_segmentation',
+        executable='laser_segmentation',
+        name='lidar_segmentation',
+        namespace='',
+        output='screen',
+        # parameters=[{
+        #     'target_frame': 'lidar_front_frame',  # Or 'base_link'
+        #     'segmentation_type': 'jump_distance_merge',
+        #     'min_points_segment': 5,
+        #     'max_distance_jump': 0.1,
+        # }],
+        parameters=[{
+            'scan_topic': '/sim/ls_front',
+            'target_frame': 'base_link',
+            'segmentation_type': 'jump_distance_merge',
+
+            # AGGRESSIVE MERGING (The "Error" Tolerance)
+            'distance_threshold': 0.4,       # Large gap tolerance between points
+            'max_distance_jump': 0.4,        # Merge segments even if they are far apart
+
+            # GENERIC FILTERING
+            'min_points_segment': 8,         # Accept even very "sparse" walls
+            'max_points_segment': 500,       # Allow long continuous hallway walls
+            'min_segment_width': 0.2,        # Don't throw away small wall chunks
+            'max_segment_width': 15.0,       # Capture the whole side of a long hall
+
+            # RANGE (Generic Hallway)
+            'min_avg_distance_from_sensor': 0.1,
+            'max_avg_distance_from_sensor': 10.0,
+        }],
+        remappings=[('/scan', '/cb/ls_front')]
+    )
+
+    # 2. Simple Command Line "Activators"
+    # We wait 2 seconds after the node starts to trigger the states
+    configure_cmd = TimerAction(
+        period=2.0,
+        actions=[
+            ExecuteProcess(
+                cmd=['ros2', 'lifecycle', 'set',
+                     '/lidar_segmentation', 'configure'],
+                output='screen'
+            )
+        ]
+    )
+
+    activate_cmd = TimerAction(
+        period=4.0,  # Wait a bit longer to ensure configuration finished
+        actions=[
+            ExecuteProcess(
+                cmd=['ros2', 'lifecycle', 'set',
+                     '/lidar_segmentation', 'activate'],
+                output='screen'
+            )
+        ]
+    )
 
     return LaunchDescription([
         DeclareLaunchArgument('use_sim_time', default_value='false'),
@@ -65,24 +125,44 @@ def generate_launch_description():
             package='simulation',
             executable='control',
             name='sim_ctrl',
-            ),
+        ),
         Node(
             package='simulation',
             executable='lidarstreams',
             name='sim_lidar',
-            ),
+        ),
         Node(
             package='simulation',
             executable='telemetry',
             name='sim_telemetry',
             parameters=[
                 {"pub_rate": 50}
-                ]
-            ),
+            ]
+        ),
         # Launch the driver board interface
         Node(
             package='control_board',
             executable='driver',
             name='cb_interface',
-            ),
+        ),
+        # Launch the LiDAR Segmentation Node
+        # Node(
+        #     package='laser_segmentation',
+        #     executable='laser_segmentation',
+        #     name='lidar_segmentation',
+        #     parameters=[{
+        #         'target_frame': 'lidar_front_frame',
+        #         'segmentation_type': 'jump_distance_merge',  # Common algorithm
+        #         'min_points': 5,
+        #         'max_distance_jump': 0.1,
+        #     }],
+        #     remappings=[
+        #         # Map 'scan' to your specific lidar topic name if it's different
+        #         ('/scan', '/cb/ls_front'),
+        #     ],
+        #     output='screen'
+        # ),
+        seg_node,
+        configure_cmd,
+        activate_cmd,
     ])
